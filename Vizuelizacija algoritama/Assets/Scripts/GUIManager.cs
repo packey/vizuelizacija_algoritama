9583dac5d899f990c3672441ec4bc6f36de6f00c
noobnakoob <aca.rampage@gmail.com>
2017-09-12 22:12:08 +0200

    IS
    Init

---
+++ Vizuelizacija algoritama/Assets/Scripts/GUIManager.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUIManager : MonoBehaviour {
+
+    [Header("Canvas Panels")]
+    public GameObject mainMenuPanel;                        // Main panel holding common buttons
+    public GameObject chooseAlgorithmPanel;                 // Choose algorithm to show 
+    public GameObject infoPanel;                            // Information about project and team
+    public GameObject exitPanel;                            // Exit confirmation panel
+
+	// Use this for initialization
+	void Start ()
+    {
+        SoundManager.instance.PlayMusic(SoundManager.instance.bgMusic);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (chooseAlgorithmPanel.activeSelf || infoPanel.activeSelf || exitPanel.activeSelf)
+                ActivatePanel(true, false, false, false);
+            else
+                ActivatePanel(false, false, false, true);
+        }
+	}
+
+    #region COMMON_BUTTONS
+
+    /// <summary>
+    /// Activates choose algorithm panel
+    /// </summary>
+    public void OnChooseAlgorithmButtonClicked()
+    {
+        SoundManager.instance.PlaySound(SoundManager.instance.clickSound);
+        ActivatePanel(false, true, false, false);
+    }
+
+    /// <summary>
+    /// Activates info panel
+    /// </summary>
+    public void OnInfoButtonClicked()
+    {
+        SoundManager.instance.PlaySound(SoundManager.instance.clickSound);
+        ActivatePanel(false, false, true, false);
+    }
+
+    /// <summary>
+    /// Activates exit panel
+    /// </summary>
+    public void OnExitButtonClicked()
+    {
+        SoundManager.instance.PlaySound(SoundManager.instance.clickSound);
+        ActivatePanel(false, false, false, true);
+    }
+
+    /// <summary>
+    /// Confirm exit from app
+    /// </summary>
+    public void OnExitYesButtonClicked()
+    {
+        SoundManager.instance.PlaySound(SoundManager.instance.clickSound);
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Decline exit from app
+    /// </summary>
+    public void ReturnToMainPanel()
+    {
+        SoundManager.instance.PlaySound(SoundManager.instance.clickSound);
+        ActivatePanel(true, false, false, false);
+    }
+    #endregion
+
+    #region CHOOSE_BUTTONS
+
+    /// <summary>
+    /// Proceed to the choosen algorithm visualization
+    /// </summary>
+    /// <param name="algorithmID"></param>
+    public void ChooseAlgorithm(int algorithmID)
+    {
+        SoundManager.instance.PlaySound(SoundManager.instance.clickSound);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + algorithmID);
+    }
+
+#endregion
+
+    #region PRIVATE_METHODS
+    /// <summary>
+    /// Activates proper panel
+    /// </summary>
+    /// <param name="main"></param>
+    /// <param name="choose"></param>
+    /// <param name="info"></param>
+    /// <param name="exit"></param>
+    private void ActivatePanel(bool main, bool choose, bool info, bool exit)
+    {
+        mainMenuPanel.SetActive(main);
+        chooseAlgorithmPanel.SetActive(choose);
+        infoPanel.SetActive(info);
+        exitPanel.SetActive(exit);
+    }
+
+#endregion
+}
---
+++ Vizuelizacija algoritama/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance;
+
+    [Header("Audio")]
+    public AudioClip clickSound;
+    public AudioClip bgMusic;
+
+    public AudioSource musicSource;
+    public AudioSource soundSource; 
+
+	// Use this for initialization
+	void Start () {
+
+        if (instance == null)
+            instance = this;
+        else
+            DontDestroyOnLoad(instance);
+	}
+	
+	
+    /// <summary>
+    /// Play sound effect
+    /// </summary>
+    /// <param name="soundClip"></param>
+    public void PlaySound(AudioClip soundClip)
+    {
+        soundSource.PlayOneShot(soundClip);
+    }
+
+    /// <summary>
+    /// Play background music
+    /// </summary>
+    /// <param name="musicClip"></param>
+    public void PlayMusic(AudioClip musicClip)
+    {
+        if (musicClip)
+        {
+            musicSource.clip = musicClip;
+            musicSource.Play();
+        }        
+    }
+}
