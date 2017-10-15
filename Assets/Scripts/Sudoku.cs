ac458d6b61fa9353857570357ab336cb8f576500
Petar Perić <petarperic93@gmail.com>
2017-10-15 23:09:02 +0200

    Ispravljem sudoku

--- Assets/Scripts/Sudoku.cs
+++ Assets/Scripts/Sudoku.cs
@@ -10,35 +10,99 @@ public class Sudoku : MonoBehaviour
     public TextMeshProUGUI infoText;
     public GameObject nextButton;
     public GameObject previewImage;
+    public float frameDurationInMS = 150f;
 
-    private int counter = 1;
+    private int counter = 0;
+    private float frameDurationInS;
 
     // Use this for initialization
     void Start()
     {
-        nextButton.GetComponent<Button>().onClick.AddListener(delegate { OnNextButtonClicked(1); });
-        counter++;
+        nextButton.GetComponent<Button>().onClick.AddListener(delegate { OnNextButtonClicked(0); });
+        frameDurationInS = frameDurationInMS * 0.001f;
     }
 
     public void OnNextButtonClicked(int nextImage)
     {
-        previewImage.GetComponent<Image>().sprite = arrayOfSprites[nextImage - 1];
+        previewImage.GetComponent<Image>().sprite = arrayOfSprites[nextImage];
                 
-        int tmpCounter = counter;
         counter++;
         nextButton.GetComponent<Button>().onClick.RemoveAllListeners();
-        nextButton.GetComponent<Button>().onClick.AddListener(delegate { OnNextButtonClicked(tmpCounter); });
+        nextButton.GetComponent<Button>().onClick.AddListener(delegate { OnNextButtonClicked(counter); });
 
         switch (nextImage)
         {
-            case 1:
-                infoText.text = "Korak1";
+            case 0:
+                counter--;
+                infoText.text = "Algoritam pokusava da dodje do resenja.";
+                StartCoroutine(chageImagePartI());
                 break;
-            case 2:
-                infoText.text = "Korak2";
+            case 9:
+                infoText.text = "Algoritam se zbog greske vratio na pocetak i sada krece iz pocetka sa novom predpostavkom.";
+                break;
+            case 10:
+                counter--;
+                infoText.text = "Algoritam pokusava da dodje do resenja. Tako da uvek kada nema moguce resenje vrace se unazad i pokusava sa novom predpostavkom.";
+                StartCoroutine(chageImagePartII());
                 break;
                 ///....
         }
         
+    }
+
+    public IEnumerator chageImagePartI()
+    {
+
+        nextButton.GetComponent<Button>().onClick.RemoveAllListeners();
+        while (true)
+        {
+            
+            if (counter == 0)
+            {
+                counter++;
+                yield return new WaitForSeconds(frameDurationInS);
+            }
+            else if (counter <  8)
+            {
+                previewImage.GetComponent<Image>().sprite = arrayOfSprites[counter];
+                counter++;
+                yield return new WaitForSeconds(frameDurationInS);
+            }
+            else
+            {
+                previewImage.GetComponent<Image>().sprite = arrayOfSprites[counter];
+                infoText.text = "Kod ovog koraka algoritam dolazi u situaciju da nema moguce resenje za sledece polje, pa se vraca unazad.";
+                counter++;
+                nextButton.GetComponent<Button>().onClick.AddListener(delegate { OnNextButtonClicked(counter); });
+                break;
+            }
+        }
+    }
+
+    public IEnumerator chageImagePartII()
+    {
+
+        nextButton.GetComponent<Button>().onClick.RemoveAllListeners();
+        while (true)
+        {
+
+            if (counter == 10)
+            {
+                counter++;
+                yield return new WaitForSeconds(frameDurationInS);
+            }
+            else if (counter < 125)
+            {
+                previewImage.GetComponent<Image>().sprite = arrayOfSprites[counter];
+                counter++;
+                yield return new WaitForSeconds(frameDurationInS);
+            }
+            else
+            {
+                previewImage.GetComponent<Image>().sprite = arrayOfSprites[counter];
+                infoText.text = "Algoritam je dosao do resenja.";
+                break;
+            }
+        }
     }
 }
